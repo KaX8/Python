59031a58f995bb3bcaded76cade5b7e6fc0559be
KaX8 <gamemode719@gmail.com>
2022-04-27 11:47:15 +0500

    all

---
+++ LabPython_1/LabPython_1/LabPython_1.py
@@ -0,0 +1,204 @@
+﻿
+
+
+def menu():
+    answ = input("Выберите задание: \n1. Максимум \n2. Положительные числа. \n3. Тип данных. \n4. Натуральное число. \n5. Треугольник. \n6. Счастливый билет. \n7. Кирпич. \n8. Шахматные фигуры. \n \n")
+    if answ == "1":
+        Max()
+    elif answ == "2":
+        Positive()
+    elif answ == "3":
+        SymType()
+    elif answ == "4":
+        Natural()
+    elif answ == "5":
+        Triangle()
+    elif answ == "6":
+        Lucky()
+    elif answ == "7":
+        Brick()
+    elif answ == "8":
+        Chess()
+    else:
+        print("Давай еще раз спросим.")
+
+    input("Продолжаем?")
+    print("\n")
+
+def Max():
+    a = []
+
+    try:
+        for i in range(3):
+            a.append(int(input("Вводите числа!!!\n")))
+        print("Максимум:",max(a))
+    except:
+        print("Вы что то не то ввели, досвидания...")
+
+def Positive():
+    answ = 0
+    try:
+        for i in range(5):
+            if int(input("Вводите числа!!!\n")) > 0: 
+                answ += 1
+        print("Положительных чисел:",answ)
+    except:
+        print("Вы что то не то ввели, досвидания...")
+
+def SymType():
+    while 1:
+        smth = input("Введите один символ!")
+        if len(smth) == 1:
+            if re.match('[0-9]', smth) is not None:
+                print(smth, "- Это число!")
+            elif re.match('[A-Z]|[a-z]|[А-Я]|[а-я]', smth) is not None:
+                print(smth, "- Это буква!")
+            elif re.match('[-.?!)(,:]', smth) is not None:
+                print(smth, "- Это знак препинания!")
+            else:
+                print(smth, "- Это что то еще!")
+            break
+        else:
+            print("Нужен только один символ!")
+    
+def Natural():
+    try:
+        a = int(input("Введите число"))
+        if a >= 0:
+            print("Число натуральное!")
+        else:
+            print("Число не натуральное!")
+    except Exception:
+        print("Это ТОЧНО не натуральное число!")
+
+def Triangle():
+    a = []
+    try:
+        print("Вводите стороны треугольника:")
+        for i in range(3):
+            a.append(int(input()))
+        if a[0] + a[1] < a[2] or a[1] + a[2] < a[0] or a[2] + a[0] < a[1]:
+            print("Такого треугольника быть не может!!")
+        else:
+            print("Да!!! Хороший треугольник!")
+    except:
+        print("Вы что то не то ввели, досвидания!")
+
+def Lucky():
+
+    while 1:
+        ticket = input("Вводите свой билетик(6 цифр):")
+        nums = []
+        a = 0
+        b = 0
+
+        if re.match("\D", ticket) is None:
+            for i in range(6):
+                nums.append(int(ticket[i]))
+            for i in range(6):
+                if i < 3:
+                    a += nums[i]
+                elif i >= 3:
+                    b += nums[i]
+            if a == b:
+                print("Поздравляю!!! У вас счастливый билетик!")
+            else:
+                print("Печаль беда. Вам не повезло...")
+            break
+        else:
+            print("Как то вы неправильно ввели... \n")
+
+def Brick():
+    brick = []
+    square = []
+    while 1:
+        try:
+            print("Вводите ребра кирпича(a,b,c) и стороны отверстия(x,y):")
+
+            print("Кирпич(a,b,c):")
+            for i in range(3):
+                brick.append(float(input()))
+
+            print("Отверстие(x,y):")
+            for i in range(2):
+                square.append(float(input()))
+
+            sBr_1 = brick[0] * brick[1]
+            sBr_2 = brick[1] * brick[2]
+            sBr_3 = brick[2] * brick[0]
+
+            sSq = square[0] * square[1]
+
+            if sSq > sBr_1 or sSq > sBr_2 or sSq > sBr_3:
+                print("Кирпич сможет пройти!")
+            else:
+                print("Не судьба, кирпич больше...")
+
+            break
+        except:
+            print("Вы что то не то ввели, давайте еще раз!\n")
+
+def Arr(num):
+    flag = True
+    a = []
+    for i in range(num):
+        a.append(input())
+    for i in range(num):
+        a[i] = int(a[i])
+        if a[i] < 1 or a[i] > 9:
+            flag = False
+    return a, flag
+
+def Chess():
+    while 1:
+        try:
+            print("Введите 2 целых числа:\nСтартовое расположение фигуры: вертикаль, горизонталь")
+            start = Arr(2)
+            print("Введите 2 целых числа:\nКуда будет шагать фигура?: вертикаль, горизонталь")
+            move = Arr(2)
+
+            if not start[1] or not move[1]:
+                print("Вы должны ввести числа от 1 до 8!")
+            else:
+                while 1:
+                    pieces = ["","Ладья","Слон","Конь","Пешка"]
+                    piece = int(input("Выберете фигуру:\n1.Ладья\n2.Слон\n3.Конь\n4.Пешка\n"))
+                    if piece >= 1 and piece <= 4:
+                        canMove = 0 #0 - не может, 1 - может, -1 = координаты одинаковые
+                        print("Сможет ли фигура \"{0}\", стоящая на координатах [{1};{2}] дойти до координат [{3};{4}]?"
+                              .format(pieces[piece], start[0][0], start[0][1], move[0][0], move[0][1]))
+
+                        if start[0] == move[0]:
+                            canMove = -1 
+                        elif piece == 1 and (start[0][0] == move[0][0] or start[0][1] == move[0][1]):#ЛАДЬЯ
+                            canMove = 1 
+                        elif piece == 2: #СЛОН
+                            if abs(start[0][0] - move[0][0]) == abs(start[0][1] - move[0][1]):
+                                canMove = 1
+                        elif piece == 3: #КОНЬ
+                            if abs(start[0][0] - move[0][0]) == 1 and abs(start[0][1] - move[0][1]) == 2 or abs(start[0][0] - move[0][0]) == 2 and abs(start[0][1] - move[0][1]) == 1:
+                                canMove = 1
+                        elif piece == 4: #ПЕШКА
+                            pawnMove = 1
+                            if start[0][1] == 2:
+                                pawnMove = 2
+                            if start[0][1] + pawnMove == move[0][1] and start[0][0] == move[0][0]:
+                                canMove = 1 
+                            
+
+                        if canMove == 0:
+                            print("Фигура НЕ сможет сходить")
+                        elif canMove == 1:
+                            print("Фигура СМОЖЕТ сходить")
+                        elif canMove == -1:
+                            print("А зачем ходить? Координаты одинаковые.")
+                        return
+                    else:
+                        print("Давайте нормальную фигуру...")
+        except ValueError:
+            print("Вы должны вводить целые числа!\n")
+
+
+if __name__ == '__main__':
+    import re
+    while 1: menu()
---
+++ LabPython_3/LabPython_3/LabPython_3.py
@@ -0,0 +1,120 @@
+﻿
+
+
+def menu():
+    answ = input("Выберите задание: \n1. Куча аргументов \n2. Сортировка \n3. Словарь \n4. Операции со словарями \n5. Сортировка с ошибкой \n6. Кортеж и элемент  \n \n")
+    if answ == "1":
+        Inf(1,2,3,4,"asdasd")
+    elif answ == "2":
+        print(Sort([4,3,2,1,10,100]))
+    elif answ == "3":
+        Dict()
+    elif answ == "4":
+        Operations()
+    elif answ == "5":
+        print(CheckSort([4,3,2,1,10,100]))
+    elif answ == "6":
+        print(DeleteFirstEl([4,3,2,1,10,100], 1))
+    else:
+        print("Давай еще раз спросим.")
+
+    input("\nПродолжаем?")
+    print("\n")
+
+class CustomError(Exception):
+    pass
+
+def Inf(*args):
+    a = []
+
+    for arg in args: 
+        a.append(arg)
+
+    print("\nАругменты:")
+    for i in range(len(a)): 
+        print(a[i])
+
+def Sort(rawList):
+    goodList = []
+    
+    while rawList:
+        minimum = rawList[0]
+        for x in rawList:
+            if x > minimum:
+                minimum = x
+
+        goodList.append(minimum)
+        rawList.remove(minimum)   
+
+    return f"\nУспешная сортировка по убыванию:\n{goodList}"
+
+def Dict():
+    d = dict.fromkeys(['one', 'two', 'three', 'four', 'five'])
+    keys = []
+    for key in d:
+        keys.append(key)
+    for i in range(len(d)):
+        d[keys[i]] = input(f"Введите значение под ключем \'{keys[i]}\'\n")
+
+    print(d)
+    input("Следующий пункт?")
+
+    buferValue = d[keys[len(keys) - 1]]
+    d[keys[len(keys) - 1]] = d[keys[0]]
+    d[keys[0]] = buferValue
+
+    print(d)
+    input("Следующий пункт?")
+
+    d.pop(keys[1])
+    print(d)
+    input("Следующий пункт?")
+
+    print("Добавляем связку \"Ключ: Значение\" ")
+    key = input("Введите ключ\n")
+    value = input("Введите значение\n")
+    d.update({key:value})
+
+    print(f"\nИтоговый словарь:\n{d}")
+
+def Operations():
+    dict_1 = {1: 12, 2: 33, 3: 10, 4: 10, 5: 2}
+    dict_2 = {1: 12, 2: 7, 3: 1, 4: 2, 5: 2}
+
+    print(f"Суммируем оддинаковые ключи: {sum_dct(dict_1, dict_2)}\n")
+    print(f"Выбираем максимум из одинаковых ключей: {max_dct(dict_1, dict_2)}")
+
+def sum_dct(*dicts):
+    return dict(reduce(lambda a, b: Counter(a) + Counter(b), dicts))
+
+def max_dct(*dicts):
+    return dict(reduce(lambda a, b: Counter(a) | Counter(b), dicts))
+
+def CheckSort(someList):
+    goodNums = True
+
+    for i in someList:
+        if type(i) is not int:
+            goodNums = False
+
+    if goodNums is False:
+        return f"\nХотя бы один элемент не является целым числом, вот вам исходный кортеж:\n{someList}"
+    else:
+        return f"\nВ кортеже находятся ТОЛЬКО целые числа{Sort(someList)}"
+
+def DeleteFirstEl(rawList, el):
+    for x in rawList:
+        if x == el:
+            rawList.remove(x) 
+            return f"\nЭлемент \'{el}\' был удален из кортежа:\n{rawList}"
+    return f"\nЭлемент \'{el}\' отсутствует:\n{rawList}"
+
+
+if __name__ == '__main__':
+    import re
+    import random
+    import math
+    from typing import Counter
+    from functools import reduce
+    while 1: menu()
+
